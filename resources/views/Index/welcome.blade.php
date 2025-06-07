3149683a7df88f10d8682f6493ffa0e5fed73582
Poolgf <23.daw.paulgracia@fundacionmontessori.com>
2025-06-07 21:52:03 +0200

    Para Presentar 2

--- resources/views/Index/welcome.blade.php
+++ resources/views/Index/welcome.blade.php
@@ -10,7 +10,31 @@
 <div class="animated-bg">
 </div>
 
-<x-header />
+<link rel="stylesheet" href="{{ asset('css/Header/header.css') }}">
+<header class="header">
+    <div class="header-logo">
+        <a href="{{ route('home') }}"><img src="{{ asset('Img/LogoPagina.png') }}" alt="Logo Pagina"></a>
+    </div>
+    <button class="ui-btn">
+        <span>
+            VLStrike
+        </span>
+    </button>
+
+    @auth
+        <div class="user-profile">
+            <a href="{{ route('jugador', Auth::id()) }}">
+                <img src="{{ Auth::user()->icono }}"
+                     alt="Foto de perfil"
+                     class="profile-photo">
+            </a>
+            <span class="username">{{ Auth::user()->nombre }}</span>
+        </div>
+    @else
+        <a href="login" class="login-button">Iniciar Sesión</a>
+    @endauth
+</header>
+
 
 <main class="main-content">
     <div class="container">
--- resources/views/RangosLOL/rangosLOL.blade.php
+++ resources/views/RangosLOL/rangosLOL.blade.php
@@ -12,7 +12,33 @@
 <a href="{{ route('home') }}" class="back-arrow">
     <i class="bi bi-arrow-left-circle-fill"></i>
 </a>
-<x-header/>
+
+<link rel="stylesheet" href="{{ asset('css/Header/header.css') }}">
+<header class="header">
+    <div class="header-logo">
+        <a href="{{ route('home') }}"><img src="{{ asset('Img/LogoPagina.png') }}" alt="Logo Pagina"></a>
+    </div>
+    <button class="ui-btn">
+        <span>
+            VLStrike
+        </span>
+    </button>
+
+    @auth
+        <div class="user-profile">
+            <a href="{{ route('jugador', Auth::id()) }}">
+                <img src="{{ Auth::user()->icono }}"
+                     alt="Foto de perfil"
+                     class="profile-photo">
+            </a>
+            <span class="username">{{ Auth::user()->nombre }}</span>
+        </div>
+    @else
+        <a href="login" class="login-button">Iniciar Sesión</a>
+    @endauth
+</header>
+
+
 <div class="container">
     <div class="rangosFondo">
         <div class="particles">
